61beb314760bca2eed30645250c28f393761996b
KintaMiao <45995071+KintaMiao@users.noreply.github.com>
2024-10-04 12:50:57 +0800

    Navbar Support

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
 "use client";
 
-import { Box, Flex, Container, Heading, VStack } from "@chakra-ui/react";
+import { Box, Flex, Container, Heading, VStack, Button, Spacer } from "@chakra-ui/react";
 import TranslationInput from "../components/TranslationInput";
 import TranslationServices from "../components/TranslationServices";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <Box minHeight="100vh" bg="gray.900" color="white">
+      <Box bg="gray.800" px={4} py={3} mb={4}>
+        <Flex alignItems="center" maxW="container.xl" mx="auto">
+          <Heading size="md">SimpleLMT</Heading>
+          <Spacer />
+          <Button as={Link} href="/" variant="ghost" mr={2}>
+            首页
+          </Button>
+          <Button as={Link} href="/about" variant="ghost">
+            关于
+          </Button>
+        </Flex>
+      </Box>
+
       <Container maxW="container.xl" py={8}>
         <VStack spacing={8}>
           <Heading as="h1" size="2xl" textAlign="center" mb={4}>
@@ -17,7 +31,7 @@ const Home = () => {
             width="100%"
             gap={8}
           >
-            <Box flex="3">
+            <Box flex="3" mr={{ md: 4 }} mb={{ base: 4, md: 0 }}>
               <TranslationInput />
             </Box>
             <Box flex="1">
